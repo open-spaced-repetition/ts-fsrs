652ac296c76484b1bff045a6c2e7dac043fa21cd
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2023-04-12 13:05:16 +0800

    add code formula comments

--- src/fsrs/fsrs.ts
+++ src/fsrs/fsrs.ts
@@ -72,10 +72,10 @@ export default class FSRS {
 
     /**
      *
-     * @param s 调度卡片
-     * @param last_d 难度
-     * @param last_s 稳定性
-     * @param retrievability 可提取性
+     * @param s scheduling Card
+     * @param last_d Difficulty
+     * @param last_s Stability
+     * @param retrievability Retrievability
      */
     next_ds(s: SchedulingCard, last_d: number, last_s: number, retrievability: number): void {
         s.again.difficulty = this.next_difficulty(last_d, Rating.Again)
@@ -89,19 +89,25 @@ export default class FSRS {
     }
 
     /**
-     * 初始化记忆稳定化衰减
-     * @param r Rating
+     * The formula used is :
+     * $$S_0(G) = w_0 + G \cdot w_1$$
+     * $$\max \{S_0,0.1\}$$
+     * @param g Grade (rating at Anki) [0.again,1.hard,2.good,3.easy]
+     * @return Stability (interval when R=90%)
      */
-    init_stability(r: number): number {
-        return Math.max(this.param.w[0] + this.param.w[1] * r, 0.1)
+    init_stability(g: number): number {
+        return Math.max(this.param.w[0] + this.param.w[1] * g, 0.1)
     }
 
     /**
-     * 初始化记忆稳定化曲线
-     * @param r Rating
+     * The formula used is :
+     * $$D_0(G) = w_2 + (G-2) \cdot w_3$$
+     * $$\min \{\max \{D_0(G),1\},10\}$$
+     * @param g Grade (rating at Anki) [0.again,1.hard,2.good,3.easy]
+     * @return Difficulty D \in [1,10]
      */
-    init_difficulty(r: number): number {
-        return Math.min(Math.max(this.param.w[2] + this.param.w[3] * (r - 2), 1), 10)
+    init_difficulty(g: number): number {
+        return Math.min(Math.max(this.param.w[2] + this.param.w[3] * (g - 2), 1), 10)
     }
 
     apply_fuzz(ivl: number) {
@@ -120,20 +126,47 @@ export default class FSRS {
     }
 
 
+    /**
+     * The formula used is :
+     * $$next_d = D + w_4 \cdot (R - 2)$$
+     * $$D^\prime(D,R) = w_5 \cdot D_0(2) +(1 - w_5) \cdot next_d$$
+     * @param d
+     * @param r Rating[0.again,1.hard,2.good,3.easy]
+     * @return next_D
+     */
     next_difficulty(d: number, r: number): number {
         const next_d = d + this.param.w[4] * (r - 2)
         return this.constrain_difficulty(this.mean_reversion(this.param.w[2], next_d))
 
     }
 
+    /**
+     * The formula used is :
+     * $$\min \{\max \{D_0,1\},10\}$$
+     */
     constrain_difficulty(difficulty: number) {
         return Math.min(Math.max(Number(difficulty.toFixed(2)), 1), 10);
     }
 
+    /**
+     * The formula used is :
+     * $$w_5 \cdot init +(1 - w_5) \cdot current$$
+     * @param init $$w_2 : D_0(2) = w_2 + (R-2) \cdot w_3= w_2$$
+     * @param current $$D + w_4 \cdot (R - 2)$$
+     * @return difficulty
+     */
     mean_reversion(init: number, current: number): number {
         return this.param.w[5] * init + (1 - this.param.w[5]) * current
     }
 
+    /**
+     * The formula used is :
+     * $$S^\prime_r(D,S,R) = S\cdot(e^{w_6}\cdot (11-D)\cdot S^{w_7}\cdot(e^{w_8\cdot(1-R)}-1)+1)$$
+     * @param d Difficulty D \in [1,10]
+     * @param s Stability (interval when R=90%)
+     * @param r Retrievability (probability of recall)
+     * @return S^\prime_r new stability after recall
+     */
     next_recall_stability(d: number, s: number, r: number): number {
         return s * (1 + Math.exp(this.param.w[6]) *
             (11 - d) *
@@ -141,6 +174,14 @@ export default class FSRS {
             (Math.exp((1 - r) * this.param.w[8]) - 1))
     }
 
+    /**
+     * The formula used is :
+     * $$S^\prime_f(D,S,R) = w_9\cdot D^{w_{10}}\cdot S^{w_{11}}\cdot e^{w_{12}\cdot(1-R)}.$$
+     * @param d Difficulty D \in [1,10]
+     * @param s Stability (interval when R=90%)
+     * @param r Retrievability (probability of recall)
+     * @return S^\prime_f new stability after forgetting
+     */
     next_forget_stability(d: number, s: number, r: number): number {
         return this.param.w[9] * Math.pow(d, this.param.w[10]) * Math.pow(s, this.param.w[11]) * Math.exp((1 - r) * this.param.w[12])
     }
