f0edba65baba90c79d07fc0cdad640d23705e86a
ishiko732 <ishiko732@gmail.com>
2023-11-20 15:48:14 +0000

    Deploying to gh-pages from @ ishiko732/ts-fsrs@bb8fc5030ed6ac34d320627dd66ec3f759036772 🚀

---
+++ example.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const { generatorParameters, fsrs, createEmptyCard, State, Rating } = tsfsrs
+
+const App = ({ cardRecord, logRecord }) => {
+    const [cards, setCards] = React.useState(cardRecord || [])
+    const [logs, setLogs] = React.useState(logRecord || [])
+    const f = fsrs(generatorParameters())
+    return <React.Fragment className="w-full">
+        <div className="text-xl text-center">Current TS-FSRS Version:{tsfsrs.FSRSVersion}</div>
+        <div className="text-lg text-center">Example</div>
+        <div className="flex">
+            <ExampleCard cardRecord={cards} f={f} className={"flex-initial w-1/2 pr-2 "}/>
+            <ExampleLog logRecord={logs} className={"flex-initial w-1/2"}/>
+        </div>
+        <div className="flex justify-center mt-8"><ExampleGenerator cards={cards} setCards={setCards} setLogs={setLogs} f={f} /></div>
+        <DefaultParams/>
+    </React.Fragment>;
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
