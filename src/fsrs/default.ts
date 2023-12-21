92ebd2389ad13f0a168ce3e1466c8a7f0fed163d
ishiko732 <ishiko732@gmail.com>
2023-12-22 00:17:47 +0800

    3.2.3

--- src/fsrs/default.ts
+++ src/fsrs/default.ts
@@ -9,7 +9,7 @@ export const default_w = [
 ];
 export const default_enable_fuzz = false;
 
-export const FSRSVersion: string = "3.2.2";
+export const FSRSVersion: string = "3.2.3";
 
 export const generatorParameters = (
   props?: Partial<FSRSParameters>,
