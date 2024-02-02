bdd6b2e949588c3529e11e68f5b67bb3b82c4175
ishiko732 <ishiko732@gmail.com>
2024-02-02 17:57:04 +0800

    Chore/remove rollup-plugin-terser

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from "@rollup/plugin-terser";
 import dts from 'rollup-plugin-dts';
 
 
@@ -34,9 +33,12 @@ export default defineConfig([
             // 对于 fs、path 等内置模块不再搜索 node_modules
             resolve({preferBuiltins: true}),
             // 设置目标为 node >= 16.0
-            esbuild({target: 'node16.0', sourceMap: true}),
+            esbuild({
+                target: 'node16.0',
+                sourceMap: true,
+                minify: true,
+            }),
             commonjs(),
-            terser()
         ],
         external: ["seedrandom"],
     },
