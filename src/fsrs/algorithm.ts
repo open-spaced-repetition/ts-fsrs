f282b5b2108d7b6d9a1b7a8c5d3203139b858cb8
ishiko <ishiko732@gmail.com>
2024-04-12 22:32:57 +0800

    Chore/config typedoc (#82)
    * Chore/config typedoc.json
    
    * update .gitignore
    
    * Doc/update algorithm.ts
    
    * Chore/inherited=false

--- src/fsrs/algorithm.ts
+++ src/fsrs/algorithm.ts
@@ -5,11 +5,21 @@ import {FSRSParameters, Grade, Rating} from "./models";
 import type { int } from "./type";
 import { get_fuzz_range } from "./help";
 
-// Ref: https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm#fsrs-v4
+/**
+ * @default DECAY = -0.5
+ */
 export const DECAY: number = -0.5;
-// FACTOR = Math.pow(0.9, 1 / DECAY) - 1
+/**
+ * FACTOR = Math.pow(0.9, 1 / DECAY) - 1= 19 / 81
+ *
+ * $$\text{FACTOR} = \frac{19}{81}$$
+ * @default FACTOR = 19 / 81
+ */
 export const FACTOR: number = 19 / 81;
 
+/**
+ * @see https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm#fsrs-45
+ */
 export class FSRSAlgorithm {
   protected param!: FSRSParameters;
   protected intervalModifier!: number;
@@ -30,9 +40,11 @@ export class FSRSAlgorithm {
   }
 
   /**
-   * Ref: https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm#fsrs-45
-   * The formula used is: I(r,s) = (r^(1/DECAY) - 1) * s / FACTOR
+   * @see https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm#fsrs-45
+   *
+   * The formula used is: $$I(r,s) = (r^{\frac{1}{DECAY}} - 1) / FACTOR \times s$$
    * @param request_retention 0<request_retention<=1,Requested retention rate
+   * @throws {Error} Requested retention rate should be in the range (0,1]
    */
   calculate_interval_modifier(request_retention: number): number {
     if (request_retention <= 0 || request_retention > 1) {
@@ -41,10 +53,17 @@ export class FSRSAlgorithm {
     return +((Math.pow(request_retention, 1 / DECAY) - 1) / FACTOR).toFixed(8);
   }
 
+  /**
+   * Get the parameters of the algorithm.
+   */
   get parameters(): FSRSParameters {
     return this.param;
   }
 
+  /**
+   * Set the parameters of the algorithm.
+   * @param params Partial<FSRSParameters>
+   */
   set parameters(params: Partial<FSRSParameters>) {
     this.update_parameters(params);
   }
@@ -132,8 +151,9 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * S_0(G) = w_{G-1}
-   * \max \{S_0,0.1\}
+   * $$ S_0(G) = w_{G-1}$$
+   * $$S_0 = \max \lbrace S_0,0.1\rbrace $$
+
    * @param g Grade (rating at Anki) [1.again,2.hard,3.good,4.easy]
    * @return Stability (interval when R=90%)
    */
@@ -143,11 +163,12 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$D_0(G) = w_4 - (G-3) \cdot w_5$$
-   * $$\min \{\max \{D_0(G),1\},10\}$$
-   * where the D_0(3)=w_4 when the first rating is good.
+   * $$D_0(G) = w_4 - (G-3) \cdot w_5 $$
+   * $$D_0 = \min \lbrace \max \lbrace D_0(G),1 \rbrace,10 \rbrace$$
+   * where the $$D_0(3)=w_4$$ when the first rating is good.
+   *
    * @param {Grade} g Grade (rating at Anki) [1.again,2.hard,3.good,4.easy]
-   * @return {number} Difficulty D \in [1,10]
+   * @return {number} Difficulty $$D \in [1,10]$$
    */
   init_difficulty(g: Grade): number {
     return +Math.min(
@@ -176,9 +197,7 @@ export class FSRSAlgorithm {
   }
 
   /**
-   *  Ref:
-   *   constructor(param: Partial<FSRSParameters>)
-   *   this.intervalModifier = 9 * (1 / this.param.request_retention - 1);
+   *   @see The formula used is : {@link FSRSAlgorithm.calculate_interval_modifier}
    *   @param {number} s - Stability (interval when R=90%)
    *   @param {number} elapsed_days t days since the last review
    *   @param {number} enable_fuzz - This adds a small random delay to the new interval time to prevent cards from sticking together and always being reviewed on the same day.
@@ -197,11 +216,11 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$next_d = D - w_6 \cdot (R - 2)$$
-   * $$D^\prime(D,R) = w_5 \cdot D_0(2) +(1 - w_5) \cdot next_d$$
-   * @param {number} d Difficulty D \in [1,10]
+   * $$\text{next}_d = D - w_6 \cdot (R - 2)$$
+   * $$D^\prime(D,R) = w_5 \cdot D_0(2) +(1 - w_5) \cdot \text{next}_d$$
+   * @param {number} d Difficulty $$D \in [1,10]$$
    * @param {Grade} g Grade (rating at Anki) [1.again,2.hard,3.good,4.easy]
-   * @return {number} next_D
+   * @return {number} $$\text{next}_D$$
    */
   next_difficulty(d: number, g: Grade): number {
     const next_d = d - this.param.w[6] * (g - 3);
@@ -212,8 +231,8 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$\min \{\max \{D_0,1\},10\}$$
-   * @param {number} difficulty D \in [1,10]
+   * $$\min \lbrace \max \lbrace D_0,1 \rbrace,10\rbrace$$
+   * @param {number} difficulty $$D \in [1,10]$$
    */
   constrain_difficulty(difficulty: number): number {
     return Math.min(Math.max(+difficulty.toFixed(8), 1), 10);
@@ -221,7 +240,7 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$w_7 \cdot init +(1 - w_7) \cdot current$$
+   * $$w_7 \cdot \text{init} +(1 - w_7) \cdot \text{current}$$
    * @param {number} init $$w_2 : D_0(3) = w_2 + (R-2) \cdot w_3= w_2$$
    * @param {number} current $$D - w_6 \cdot (R - 2)$$
    * @return {number} difficulty
@@ -234,7 +253,7 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$S^\prime_r(D,S,R,G) = S\cdot(e^{w_8}\cdot (11-D)\cdot S^{-w_9}\cdot(e^{w_10\cdot(1-R)}-1)\cdot w_15(if G=2) \cdot w_16(if G=4)+1)$$
+   * $$S^\prime_r(D,S,R,G) = S\cdot(e^{w_8}\cdot (11-D)\cdot S^{-w_9}\cdot(e^{w_{10}\cdot(1-R)}-1)\cdot w_{15}(\text{if} G=2) \cdot w_{16}(\text{if} G=4)+1)$$
    * @param {number} d Difficulty D \in [1,10]
    * @param {number} s Stability (interval when R=90%)
    * @param {number} r Retrievability (probability of recall)
@@ -258,7 +277,7 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$S^\prime_f(D,S,R) = w_11\cdot D^{-w_{12}}\cdot ((S+1)^{w_{13}}-1) \cdot e^{w_{14}\cdot(1-R)}.$$
+   * $$S^\prime_f(D,S,R) = w_{11}\cdot D^{-w_{12}}\cdot ((S+1)^{w_{13}}-1) \cdot e^{w_{14}\cdot(1-R)}$$
    * @param {number} d Difficulty D \in [1,10]
    * @param {number} s Stability (interval when R=90%)
    * @param {number} r Retrievability (probability of recall)
@@ -275,7 +294,7 @@ export class FSRSAlgorithm {
 
   /**
    * The formula used is :
-   * $$R(t,S) = (1 + FACTOR \times \frac{t}{9 \cdot S})^{DECAY},$$
+   * $$R(t,S) = (1 + \text{FACTOR} \times \frac{t}{9 \cdot S})^{\text{DECAY}}$$
    * @param {number} elapsed_days t days since the last review
    * @param {number} stability Stability (interval when R=90%)
    * @return {number} r Retrievability (probability of recall)
