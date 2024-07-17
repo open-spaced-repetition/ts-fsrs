d4b829e39932e17dd2108b4e50e39b9c14fe22c1
ishiko <ishiko732@gmail.com>
2024-07-18 00:38:24 +0800

    add interface

--- src/fsrs/types.ts
+++ src/fsrs/types.ts
@@ -1,3 +1,36 @@
+import type {
+  Card,
+  CardInput,
+  DateInput,
+  Grade,
+  RecordLog,
+  RecordLogItem,
+} from './models'
+
 export type unit = 'days' | 'minutes'
 export type int = number & { __int__: void }
 export type double = number & { __double__: void }
+
+export interface IScheduler {
+  init(card: Card, now: Date): this
+  preview(): RecordLog
+  review(state: Grade): RecordLogItem
+  addPlugin(plugin: ISchedulerLifecycle): this
+}
+
+/**
+ * @todo
+ */
+export interface ISchedulerLifecycle {
+  init(): void
+  update_state(): void
+  schedule(): void
+  display(): void
+}
+
+export interface ITransformer {
+  card<T extends Card | CardInput>(card: T): Card
+  recordLog<T extends RecordLog>(recordLog: T): RecordLog
+  recordLogItem<T extends RecordLogItem>(recordLogItem: T): RecordLogItem
+  time<T extends Date | DateInput>(time: T): Date
+}
