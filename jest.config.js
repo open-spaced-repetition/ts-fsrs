c99671c417aa09567bd253febc01e42c4e89e853
ishiko <ishiko732@gmail.com>
2024-07-20 23:08:24 +0800

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/*.js?(x)", "**/__tests__/*.ts?(x)"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/*.ts?(x)',
+    '**/__tests__/**/*.ts?(x)',
+  ],
   collectCoverage: true,
-  coverageReporters: ["text", "cobertura"],
+  coverageReporters: ['text', 'cobertura'],
   coverageThreshold: {
     global: {
       lines: 80,
     },
   },
   transformIgnorePatterns: ['/node_modules/(?!(module-to-transform)/)'],
-};
+}
