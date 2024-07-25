8966179018ff6a583191f9cf06414a62e759575c
ishiko <ishiko732@gmail.com>
2024-07-25 22:45:22 +0800

    bump ts-fsrs to 4.0.0

--- src/fsrs/default.ts
+++ src/fsrs/default.ts
@@ -11,7 +11,7 @@ export const default_w = [
 export const default_enable_fuzz = false
 export const defualt_enable_short_term = true
 
-export const FSRSVersion: string = 'v3.5.7 using FSRS V5.0'
+export const FSRSVersion: string = 'v4.0.0 using FSRS V5.0'
 
 export const generatorParameters = (
   props?: Partial<FSRSParameters>
