b439c7890278fd83e7b33d2baef50d17530417f5
ishiko <ishiko732@gmail.com>
2025-01-18 23:59:48 +0800

    update rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,76 +1,79 @@
-import {defineConfig} from 'rollup';
-import resolve from '@rollup/plugin-node-resolve';
-import esbuild from 'rollup-plugin-esbuild';
-import commonjs from '@rollup/plugin-commonjs';
-import dts from 'rollup-plugin-dts';
+import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import esbuild from 'rollup-plugin-esbuild'
+import commonjs from '@rollup/plugin-commonjs'
+import dts from 'rollup-plugin-dts'
+
+const isDev = process.env.NODE_ENV === 'development'
+const minify = isDev ? false : true
 
 export default defineConfig([
-    {
-        input: {
-            index: 'src/fsrs/index.ts',
-        },
-        output: [
-            {
-                dir: 'dist',
-                entryFileNames: '[name].cjs',
-                format: 'cjs',
-                sourcemap: true,
-                exports: 'named',
-                footer: ({exports}) =>
-                    exports.length > 0
-                        ? 'module.exports = Object.assign(exports.default || {}, exports)'
-                        : '',
-            },
-            {
-                dir: 'dist',
-                entryFileNames: '[name].mjs',
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve({preferBuiltins: true}),
-            esbuild({
-                target: 'node18.0',
-                sourceMap: true,
-                minify: true,
-            }),
-            commonjs(),
-        ],
-        external: [],
-    }, 
-    {
-        input: 'src/fsrs/index.ts',
-        output: {
-            file: 'dist/index.umd.js', 
-            format: 'umd',
-            name: 'FSRS',
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(), 
-            esbuild({ 
-                target: 'es2017',
-                minify: true,
-                sourceMap: true,
-            }),
-            commonjs(),
-        ],
-        external: [],
+  {
+    input: {
+      index: 'src/fsrs/index.ts',
     },
-    {
-        input: 'src/fsrs/index.ts',
-        output: {
-            file: 'dist/index.d.ts',
-            format: 'esm'
-        },
-        plugins: [
-            dts({
-                // https://github.com/Swatinem/rollup-plugin-dts/issues/143
-                compilerOptions: {preserveSymlinks: false},
-                respectExternal: true,
-            }),
-        ],
-        external: [],
+    output: [
+      {
+        dir: 'dist',
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+        footer: ({ exports }) =>
+          exports.length > 0
+            ? 'module.exports = Object.assign(exports.default || {}, exports)'
+            : '',
+      },
+      {
+        dir: 'dist',
+        entryFileNames: '[name].mjs',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({ preferBuiltins: true }),
+      esbuild({
+        target: 'node18.0',
+        sourceMap: true,
+        minify: minify,
+      }),
+      commonjs(),
+    ],
+    external: [],
+  },
+  {
+    input: 'src/fsrs/index.ts',
+    output: {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'FSRS',
+      sourcemap: true,
     },
-]);
+    plugins: [
+      resolve(),
+      esbuild({
+        target: 'es2017',
+        minify: minify,
+        sourceMap: true,
+      }),
+      commonjs(),
+    ],
+    external: [],
+  },
+  {
+    input: 'src/fsrs/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'esm',
+    },
+    plugins: [
+      dts({
+        // https://github.com/Swatinem/rollup-plugin-dts/issues/143
+        compilerOptions: { preserveSymlinks: false },
+        respectExternal: true,
+      }),
+    ],
+    external: [],
+  },
+])
