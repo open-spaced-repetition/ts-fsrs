8d95d3534ac0801af62fb70efe1270b5728df3e5
ishiko <ishiko732@gmail.com>
2025-05-05 16:48:29 +0800

    impl learning steps strategy

--- src/fsrs/strategies/types.ts
+++ src/fsrs/strategies/types.ts
@@ -1,10 +1,18 @@
 import type { AbstractScheduler } from '../abstract_scheduler'
 import type { FSRSAlgorithm } from '../algorithm'
-import type { Card, CardInput, DateInput } from '../models'
+import type {
+  Card,
+  CardInput,
+  DateInput,
+  FSRSParameters,
+  Grade,
+  State,
+} from '../models'
 import type { IScheduler } from '../types'
 
 export enum StrategyMode {
   SCHEDULER = 'Scheduler',
+  LEARNING_STEPS = 'LearningSteps',
   SEED = 'Seed',
 }
 
@@ -14,12 +22,23 @@ export type TSchedulerStrategy<T extends CardInput | Card = CardInput | Card> =
     card: T,
     now: DateInput,
     algorithm: FSRSAlgorithm,
-    strategies: { seed: TSeedStrategy }
+    strategies: Map<StrategyMode, TStrategyHandler>
   ) => IScheduler
 
-export type TStrategyHandler<E = StrategyMode> =
-  E extends StrategyMode.SCHEDULER
-    ? TSchedulerStrategy
-    : E extends StrategyMode.SEED
-      ? TSeedStrategy
-      : never
+export type TLearningStepsStrategy = (
+  params: FSRSParameters,
+  state: State,
+  cur_step: number
+) => {
+  [K in Grade]?: { scheduled_minutes: number; next_step: number }
+}
+
+type StrategyMap = {
+  [StrategyMode.SCHEDULER]: TSchedulerStrategy
+  [StrategyMode.SEED]: TSeedStrategy
+  [StrategyMode.LEARNING_STEPS]: TLearningStepsStrategy
+}
+
+export type TStrategyHandler<E = StrategyMode> = E extends StrategyMode
+  ? StrategyMap[E]
+  : never
