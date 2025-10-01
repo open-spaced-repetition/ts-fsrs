1fa32c95a41ae3d08ac94ec4acd18b4a159412e9
ishiko <ishiko732@gmail.com>
2025-10-01 12:42:49 +0800

    remove console.log

--- packages/binding/__tests__/test.ts
+++ packages/binding/__tests__/test.ts
@@ -2,6 +2,5 @@ import { plus100 } from '@open-spaced-repetition/binding'
 
 test('sync function from native code', () => {
   const fixture = 42
-  console.log(plus100(fixture))
   expect(plus100(fixture)).toBe(fixture + 100)
 })
