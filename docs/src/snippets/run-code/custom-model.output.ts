6912de217ac96586b46936f91130a6c235dcb720
ishiko <ishiko732@gmail.com>
2026-09-05 14:43:07 +0900

    docs: document FSRS model formulas

---
+++ docs/src/snippets/run-code/custom-model.output.ts
@@ -0,0 +1,17 @@
+export default `{
+  "config": {
+    "weights": [
+      1,
+      0.5
+    ]
+  },
+  "firstReview": {
+    "stability": 3,
+    "difficulty": 4
+  },
+  "secondReview": {
+    "stability": 3.45,
+    "difficulty": 4
+  },
+  "interval": 3
+}`
