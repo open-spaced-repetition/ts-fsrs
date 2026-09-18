0b7a851679e560e52175345388d715188005c8e9
ishiko <ishiko732@gmail.com>
2026-09-18 17:34:23 +0900

    fix(docs): restart SM2 repetitions after a lapse

--- docs/src/snippets/run-code/custom-model.output.ts
+++ docs/src/snippets/run-code/custom-model.output.ts
@@ -13,5 +13,20 @@ export default `[
     "interval": 15,
     "easeFactor": 2.5,
     "repetitions": 3
+  },
+  {
+    "interval": 1,
+    "easeFactor": 2.18,
+    "repetitions": 0
+  },
+  {
+    "interval": 1,
+    "easeFactor": 2.18,
+    "repetitions": 1
+  },
+  {
+    "interval": 6,
+    "easeFactor": 2.18,
+    "repetitions": 2
   }
 ]`
