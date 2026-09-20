915e684f8b1900a1877026d39dea62880cacf19d
ishiko <ishiko732@gmail.com>
2026-09-20 10:08:50 +0800

    feat(middleware): add Cost ADR scheduling middleware (#528)
    * feat(middleware): add Cost ADR scheduling
    
    * feat(middleware): export Cost ADR default constants

---
+++ packages/fsrs/src/middlewares/cost-adr/index.ts
@@ -0,0 +1,11 @@
+export {
+  COST_ADR_DEFAULT_GOAL_COST_WEIGHT,
+  COST_ADR_DEFAULT_POLICY,
+} from './constants.js'
+export { schedulerCostAdrMiddleware } from './middleware.js'
+export {
+  type CostAdrConfig,
+  type CostAdrPolicy,
+  costAdrConfigSchema,
+  costAdrPolicySchema,
+} from './schema.js'
