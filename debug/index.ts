786e95da6d6ef1dda807c0c2f98875e33a8603f6
ishiko <ishiko732@gmail.com>
2026-09-24 18:29:14 +0900

    chore: remove obsolete debug scripts

--- debug/index.ts
+++
@@ -1,9 +0,0 @@
-import { FSRSVersion } from 'ts-fsrs'
-import { runLongTerm } from './long-term'
-import { runShortTerm } from './short-term'
-
-console.log(FSRSVersion)
-
-
-runShortTerm()
-runLongTerm()
